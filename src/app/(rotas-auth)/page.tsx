bd4eaabc2ec331ffc8cc315289610b5beb08ced4
Victor Abreu <aalevictor@gmail.com>
2024-03-04 23:27:28 -0300

    suspense home

--- src/app/(rotas-auth)/page.tsx
+++ src/app/(rotas-auth)/page.tsx
@@ -1,8 +1,17 @@
 'use client'
 
 import Content from '@/components/Content';
+import { Suspense } from 'react';
 
 export default function Home() {
+  return (
+    <Suspense>
+      <SearchHome />
+    </Suspense>
+  )
+}
+
+function SearchHome() {
   return (
     <Content
       titulo='Página Inicial'
