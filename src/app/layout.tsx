b2a49dfa7c717a8e7e33771c2e56473422406e81
Victor Abreu <aalevictor@gmail.com>
2024-05-29 12:50:34 -0300

    correção de carregamento de fonte

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,13 @@ import {
   THEME_ID as MATERIAL_THEME_ID,
 } from '@mui/material/styles';
 import { CssVarsProvider as JoyCssVarsProvider, extendTheme } from '@mui/joy/styles';
+import { Lato } from "next/font/google";
+
+const lato = Lato({
+  weight: ["300", "400", "700", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 const materialTheme = materialExtendTheme({
   colorSchemes: {
@@ -50,8 +57,8 @@ const materialTheme = materialExtendTheme({
 
 const theme = extendTheme({
   fontFamily: {
-    display: 'Pangram', // applies to `h1`–`h4`
-    body: 'Lato', // applies to `title-*` and `body-*`
+    display: lato.style.fontFamily, // applies to `h1`–`h4`
+    body: lato.style.fontFamily, // applies to `title-*` and `body-*`
   },
   "colorSchemes": {
     "dark": {
@@ -119,11 +126,6 @@ export default function RootLayout({children}:{children: React.ReactNode}) {
         <MenuProvider>
           <html lang="pt-BR">
             <head>
-              <link type='text/css' rel='stylesheet' href='global.css' />
-              <link rel="preconnect" href="https://fonts.googleapis.com" />
-              <link rel="preconnect" href="https://fonts.gstatic.com" />
-              <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
-              {/* <link href="https://db.onlinewebfonts.com/c/56a82ed18dbcec3eaab8c012dea226cf?family=Pangram" rel="stylesheet" /> */}
               <title>{process.env.NEXT_PUBLIC_PROJECT_NAME}</title>
             </head>
             <body>
